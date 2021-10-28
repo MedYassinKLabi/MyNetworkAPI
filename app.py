cb71049f2f7b5056d01fe84c8a403d72487fd4ce
unknown <mohamedyassin.klabi@polytechnicien.tn>
2021-10-28 22:59:23 +0100

    changement

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ def predict():
 
 @app.route('/')
 def root():
-    return "Bienvenue à Mynetwork API, crée par Klabi Med Yassin ingénieur GTR"
+    return "Bienvenue à Mynetwork API"
 
 
 if __name__ == '__main__':
